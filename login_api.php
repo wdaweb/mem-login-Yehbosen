0ac4c25ce81c00c0df1a66ae34172aa0464adfc3
Yehbosen <sulimase28@gmail.com>
2019-10-31 09:19:18 +0800

    20191031

--- login_api.php
+++ login_api.php
@@ -7,7 +7,48 @@
  * 4.比對表單資料和資料庫資料是否一致
  * 5.根據比對的結果決定畫面的行為
   ***************************************************/
-
+  $acc=$_POST['acc'];
+  $pw=$_POST['pw'];
+  
+  echo "acc=".$acc;
+  echo "<br>";
+  echo "pw=".$pw;
+  
+  $dsn="mysql:host=localhost;charset=utf8;dbname=mydb";
+  $pdo=new PDO($dsn,'root','1740');
+  
+  $sql="select count(*) as 'r' from user where acc='$acc' &&  pw='$pw'";
+  
+  // $data=$pdo->query($sql)->fetchColumn();
+  $data=$pdo->query($sql)->fetch();
+  
+  
+  print_r($data);
+  
+  if($data['r']==1){
+    echo "登入成功";
+  }else{
+    echo "登入失敗";
+  }
+  
+  
+  
+  
+  
+  
+  /*if(!empty($date)){
+    echo "登入成功";
+  }else{
+    echo "登入失敗";
+  }*/
+  
+  /*if($acc==$data['acc'] && $pw=$data['pw']){
+    echo "登入成功";
+  }else{
+    echo "登入失敗";
+  }*/
+  
+  
 
 
 
--- reg_api.php
+++ reg_api.php
@@ -8,9 +8,31 @@
  * 5.執行資料庫連線並送出SQL語法
  * 6.判斷SQL語法是否執行成功，執行下一步
  ***************************************************/
+echo $acc=$_POST['acc'];
+echo $pw=$_POST['pw'];
+echo $name=$_POST['name'];
+echo $addr=$_POST['addr'];
+echo $tel=$_POST['tel'];
+echo $date=$_POST['date'];
+echo $email=$_POST['email'];
 
+// insert into user () values();
+$dsn="mysql:host=localhost;charset=utf8;dbname=mydb";
+$pdo=new PDO($dsn,'root','1740');
 
+$sql="insert into user (`acc`,`pw`,`name`,`addr`,`tel`,`birthay`,`email`) values
+('$acc','$pw','$name','$addr','$tel','$date','$email')";
+echo "sql語法是:".$sql;
+// echo $pdo->query($sql);
 
+// $pdo->exec($sql) 用在不需要回傳資料的場景 del,update,insert
+if($pdo->exec($sql)){
+    // echo "新增資料成功";
+    header("location:index.php?s=1");
+}else{
+    // "新增失敗,請洽資料庫管理人員";
+    header("location:reg.php?s=2");
+}
 
 
 ?>
